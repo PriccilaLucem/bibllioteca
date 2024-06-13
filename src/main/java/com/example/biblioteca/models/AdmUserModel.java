6c8f7013a063640db03d82a14cc8ada9ac2ef716
Priccila Lucem <priccilalucem@gmail.com>
2024-06-13 11:24:10 -0300

    feat/usersModels

---
+++ src/main/java/com/example/biblioteca/models/AdmUserModel.java
@@ -0,0 +1,56 @@
+package com.example.biblioteca.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "adm")
+public class AdmUserModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "email", nullable = false, length = 60 )
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 60)
+    private String password;
+    
+    @Column(name = "is_adm", nullable = false)
+    private Boolean isAdm;
+
+    public AdmUserModel(){}
+
+    public AdmUserModel(String email, String password, Boolean isAdm) {
+        this.email = email;
+        this.password = password;
+        this.isAdm = true;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getId() {
+        return id;
+    }
+    public Boolean getIsAdm() {
+        return isAdm;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setIsAdm(Boolean isAdm) {
+        this.isAdm = isAdm;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
---
+++ src/main/java/com/example/biblioteca/models/UserModel.java
@@ -0,0 +1,18 @@
+package com.example.biblioteca.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class UserModel extends AdmUserModel {
+    
+    public UserModel() {
+        super();
+        setIsAdm(false);
+    }
+    
+    public UserModel(String password, String email){
+        super(email, password, false);
+    }
+}
